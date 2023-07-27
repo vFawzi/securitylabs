e74d427dd669676fac13d0d1b7db54036cce528e
vFawzi <fawzi84@gmail.com>
2023-07-27 12:43:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 sudo apt update 
 sudo apt install python3-pip -y
 sudo pip3 install flask
-curl https://raw.githubusercontent.com/JOSHUAJEBARAJ/hack/main/test.py -o test.py
+curl https://raw.githubusercontent.com/vFawzi/securitylabs/main/test.py -o test.py
 sudo python3 test.py
